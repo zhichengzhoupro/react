1b16cbcc7763f121109bf1308b8d06fa003f9a08
zhicheng <zhichengzhouenfrance@gmail.com>
2020-03-21 17:27:33 +0100

    sample projet

---
+++ antd-demo/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Button} from "antd";
+function App() {
+  return (
+    <div className="App">
+      <Button type={"primary"}> hello </Button>
+    </div>
+  );
+}
+
+export default App;
