8e61d2c303cbda8f5c44f412af4119758bacb598
zhicheng <zhichengzhouenfrance@gmail.com>
2020-03-21 18:41:03 +0100

    success

--- antd-demo/src/App.tsx
+++ antd-demo/src/App.tsx
@@ -1,23 +1,27 @@
 import React, {Fragment} from 'react';
 import './App.css';
-import {HashRouter as router, Route, Router, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'
 import {mainRouter} from './routers/Router'
 import { createBrowserHistory } from 'history';
+import Admin from "./views/admin/Admin";
 
 const history = createBrowserHistory();
 
 function App() {
   return (
       <Fragment>
-          <Router history={history}>
-              <switch>
-                  <Route path="/admin" render={(routerProps) => {return <App {...routerProps}/>}} />
+          <Router>
+              <Switch>
+                  <Route path={'/admin'}  render={(routeProps) => {
+                      return <Admin {...routeProps}></Admin>
+                  }}></Route>
                   {
-                      mainRouter.map(router => {
-                          return <Route path={route.pathName} component={route.component}></Route>
-                      })
+                      mainRouter.map(route => {
+                          return <Route path={route.pathName} component={route.component}></Route>})
                   }
-              </switch>
+                  <Redirect to="/admin" from="/" exact></Redirect>
+                  <Redirect to="/404"></Redirect>
+              </Switch>
           </Router>
       </Fragment>
 
