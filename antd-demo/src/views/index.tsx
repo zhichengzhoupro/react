bc7ae15c0505a5f6890b97aded609f38dcd615f3
zhicheng <zhichengzhouenfrance@gmail.com>
2020-03-22 13:27:11 +0100

    P45 finish

--- antd-demo/src/views/index.tsx
+++ antd-demo/src/views/index.tsx
@@ -1,10 +1,37 @@
-import DashBoard from "./dashboard/DashBoard";
-import ArticleEdit from "./Article/ArticleEdit";
-import Article from "./Article/Article";
-import NotFound from "./not-found/NotFound";
-import Login from "./login/Login";
-import Settings from "./settings/Settings";
+//import Loadable from'react-loadable'
+import { Loading } from '../components/index'
+import Loadable from "./Loadable"
 
+
+const DashBoard = Loadable({
+    Loader: () => import('./dashboard/DashBoard'),
+    Loading: Loading
+})
+
+const ArticleEdit = Loadable({
+    Loader: () => import('./Article/ArticleEdit'),
+    Loading: Loading
+})
+
+const Article = Loadable({
+    Loader: () => import('./Article/Article'),
+    Loading: Loading
+})
+
+const NotFound = Loadable({
+    Loader: () => import('./not-found/NotFound'),
+    Loading: Loading
+})
+
+const Login = Loadable({
+    Loader: () => import('./login/Login'),
+    Loading: Loading
+})
+
+const Settings = Loadable({
+    Loader: () => import('./settings/Settings'),
+    Loading: Loading
+})
 
 export {
     DashBoard,
