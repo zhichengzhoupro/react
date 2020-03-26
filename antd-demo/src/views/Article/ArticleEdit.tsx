a4cfb351189e40eb063e1f386af8fabbc2d1f028
zhicheng <zhichengzhouenfrance@gmail.com>
2020-03-26 10:08:14 +0100

    P60 finish

--- antd-demo/src/views/Article/ArticleEdit.tsx
+++ antd-demo/src/views/Article/ArticleEdit.tsx
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
-import {Button, Card, Checkbox, Form, Input} from "antd";
+import {Button, Card, Checkbox, DatePicker, Form, Input, InputNumber} from "antd";
 import {RouteComponentProps, withRouter} from 'react-router-dom';
 import {getArticleById} from "../../services/service";
 import {FormProps} from "antd/es/form";
 import FormItem from "antd/es/form/FormItem";
+import moment from "moment";
 
 type state = {
     title: string
@@ -22,16 +23,31 @@ interface ArticleEditProps extends RouteComponentProps<MatchParams, context, sta
 
 
 interface ArticleState {
-
+    titleHelp?: string
+    validateStatus?: any
 }
+
+const layout = {
+    labelCol: { span: 2 },
+    wrapperCol: { span: 12 },
+};
+const tailLayout = {
+    wrapperCol: { offset: 2, span: 12 },
+};
+
+const dateFormat = 'YYYY/MM/DD';
+
 class ArticleEdit extends Component<ArticleEditProps, ArticleState> {
 
     constructor(articleEditProps: ArticleEditProps) {
         super(articleEditProps);
+        this.state = {
+            titleHelp : 'test',
+            validateStatus: 'success'
+        }
     }
 
     componentDidMount(): void {
-        console.log(this.props);
         getArticleById(this.props.match.params.id).then((Response) => {
 
         });
@@ -49,53 +65,108 @@ class ArticleEdit extends Component<ArticleEditProps, ArticleState> {
         console.log('Failed:', errorInfo);
     };
 
+    private validateUserName = (_: any, value: string) => {
+        if(value && value.indexOf('@') === -1) {
+            this.setState({
+                titleHelp: 'Should contain @',
+                validateStatus: 'error'
+            })
+        } else  {
+            this.setState({
+                validateStatus: 'success'
+            });
+         return Promise.resolve();
+        }
+
+    }
+
+    private onDateChange = () => {
+
+    }
 
     render() {
-        const layout = {
-            labelCol: { span: 8 },
-            wrapperCol: { span: 16 },
-        };
 
-        const tailLayout = {
-            wrapperCol: { offset: 8, span: 16 },
-        };
 
         return (
+
             <Card
                 title={this.props.location.state ? this.props.location.state.title : 'Edit Article'}
-                extra={<Button type={"primary"}> 保存 </Button>}
+                extra={<Button type={"primary"}> EXIT </Button>}
             >
                 <Form
                     name="basic"
                     initialValues={{ remember: true }}
                     onFinish={this.onFinish}
                     onFinishFailed={this.onFinishFailed}
+                    {...layout}
+
                 >
-                    <FormItem
-                        label="Username"
-                        name="username"
-                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    <Form.Item
+                        label="title"
+                        name="title"
+                        validateStatus={this.state.validateStatus}
+                        rules={
+                            [
+                                {
+                                    required: true,
+                                    message: 'Please input Article title!'
+                                },
+
+                                {
+                                    validator: this.validateUserName
+                                }
+
+                            ]
+                        }
                     >
                         <Input />
-                    </FormItem>
-
-                    <FormItem
-                        label="Password"
-                        name="password"
-                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    </Form.Item>
+                    <Form.Item
+                        label="author"
+                        name="author"
+                        rules={
+                            [
+                                {
+                                    required: true,
+                                    message: 'Please input Article author!'
+                                }
+                            ]
+                        }
                     >
-                        <Input.Password />
-                    </FormItem>
-
-                    <FormItem {...tailLayout} name="remember" valuePropName="checked">
-                        <Checkbox>Remember me</Checkbox>
-                    </FormItem>
-
-                    <FormItem {...tailLayout}>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="amount"
+                        name="amount"
+                        rules={
+                            []
+                        }
+                    >
+                        <InputNumber />
+                    </Form.Item>
+                    <Form.Item
+                        label="Create At"
+                        name="createAt"
+                        rules={
+                            []
+                        }
+                    >
+                        <DatePicker showTime placeholder={"select time"}  onChange={this.onDateChange} />
+                    </Form.Item>
+                    <Form.Item
+                        label="Content"
+                        name="content"
+                        rules={
+                            []
+                        }
+                    >
+                        <Input.TextArea allowClear={true} />
+                    </Form.Item>
+                    <Form.Item {...tailLayout}>
                         <Button type="primary" htmlType="submit">
                             Submit
                         </Button>
-                    </FormItem>
+                    </Form.Item>
                 </Form>
             </Card>
         );
